686149e9624a8dc5fcc253954f822e6b87aacf41
Iyarr <8.24ridiculous@gmail.com>
2025-01-29 13:16:38 +0900

    grafanaの構築

---
+++ etc/broadcast_storm/grafana.tf
@@ -0,0 +1,27 @@
+resource "docker_image" "grafana" {
+  name = "grafana/grafana:latest"
+}
+
+resource "local_file" "grafana_datasource" {
+  content = templatefile("./temp/grafana/datasource", {
+    prometheus_url = "http://${docker_container.prometheus.network_data[0].ip_address}:${var.prometheus.port}",
+  })
+
+  filename = "local/datasource.yml"
+}
+
+resource "docker_container" "grafana" {
+  name  = "grafana"
+  image = docker_image.grafana.image_id
+  networks_advanced {
+    name = docker_network.prometheus.name
+  }
+  upload {
+    source = local_file.grafana_datasource.filename
+    file   = "/etc/grafana/provisioning/datasources/datasource.yml"
+  }
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
